0988fa50a624ef5f678f49198a37f4c8a5446e19
7apri <papri123465@gmail.com>
2026-01-06 21:50:04 +0100

    Fixed my pickaxe

--- js/util/theme.js
+++ js/util/theme.js
@@ -1,48 +1,11 @@
 const THEME_KEY = 'theme';
-<<<<<<< HEAD
-=======
 let metaEL = document.querySelector('meta[name="theme-color"]');
 const root = document.documentElement;
->>>>>>> f49c7b5 (did some offline mining)
 
 function updateThemeButton(btn, theme) {
   if (!btn) return;
   const next = theme === 'light' ? 'dark' : 'light';
   btn.setAttribute('aria-pressed', theme === 'light' ? 'true' : 'false');
-<<<<<<< HEAD
-
-  const img = btn.querySelector('img');
-  if (img) {
-    const iconByNextMode = {
-      light: './assets/Icons/lightbulb-solid-full.svg',
-      dark: './assets/Icons/moon-regular-full.svg'
-    };
-    img.src = iconByNextMode[next] || img.src;
-    img.alt = `switch to ${next} mode`;
-  }
-}
-
-export function applyTheme(theme) {
-  const root = document.documentElement;
-  if (theme === 'light') root.setAttribute('data-theme', 'light');
-  else root.removeAttribute('data-theme'); // default = dark
-
-  localStorage.setItem(THEME_KEY, theme);
-  updateThemeButton(document.querySelector('.mode-btn'), theme);
-}
-
-export function applyThemeFromButton(theme, btn = document.querySelector('.mode-btn')) {
-  applyTheme(theme);
-  updateThemeButton(btn, theme);
-}
-
-export function getSystemTheme() {
-  const saved = localStorage.getItem(THEME_KEY);
-  if (saved) return saved;
-  return window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
-}
-
-=======
   btn.setAttribute('aria-label',next === "light" ? "Přepnout na světlý režim" : "Přepnout na tmavý režim" )
 
   const img = btn.querySelector('use');
@@ -99,4 +62,3 @@ export default function setupTheme(){
     setupThemeBtn();
   }
 }
->>>>>>> f49c7b5 (did some offline mining)
